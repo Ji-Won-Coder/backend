5e4c95e40a070a8f2ea1aa2ff2fb1999b92e4f2f
Mary <maria.martinez580@misena.edu.co>
2023-05-05 12:55:36 -0500

    dependencies-databases

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST, 
+      port: +process.env.DB_PORT, //convierto el puerto en numero con el + porque el puerto es un numero
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      autoLoadEntities: true, //cargue de las entidades 
+      synchronize: true, //sincriniza 
+    }),
+  ],
   
 })
 export class AppModule {}
