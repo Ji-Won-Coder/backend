c79cf7ae03c1e2b26a0255906adfb4dfe74d567f
Mary <maria.martinez580@misena.edu.co>
2023-05-17 17:09:07 -0500

    manejo de errores y subir informacion a la bd

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,19 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException,Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { InjectRepository } from '@nestjs/typeorm/dist/common';
 import { Repository } from 'typeorm';
 
+
 @Injectable()
 export class ProductsService {
 
+  private readonly logger = new Logger('ProductsService');
+
+
   constructor(
+
 
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
@@ -17,13 +22,20 @@ export class ProductsService {
   
     async create(createProductDto: CreateProductDto) {
     try {
+
+      if(!createProductDto.slug){
+        createProductDto.slug =createProductDto.title.toLocaleLowerCase().replaceAll(' ','_').replaceAll("'",' ')
+      }
+
+
       
       const product= this.productRepository.create(createProductDto) //crear un producto con sus propiedades
       await this.productRepository.save(product); //guarda el producto en bd
+
       return product;
+
     } catch (error) {
-      console.log(error)
-      throw new InternalServerErrorException('Ayuda!');
+      this.handleDBExceptions(error);
     }
     }
   
@@ -43,7 +55,14 @@ export class ProductsService {
       return `This action removes a #${id} product`;
     }
 
-  
+    //ERROR CONTROLADO
+  private handleDBExceptions(error:any){//todo tipo de error
+    if(error.code == '23505' )
+      throw new BadRequestException(error.detail);
+
+      this.logger.error(error)
+      throw new InternalServerErrorException('Unexpected error, check server logs');
+  }
 }
 
   
