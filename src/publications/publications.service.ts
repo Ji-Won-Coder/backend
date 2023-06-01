51672a438e7c49176bd1c7d6a3611a83b6b7ca35
Mary <maria.martinez580@misena.edu.co>
2023-06-01 08:01:50 -0500

    error  TS2769

--- src/publications/publications.service.ts
+++ src/publications/publications.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePublicationDto } from './dto/create-publication.dto';
 import { UpdatePublicationDto } from './dto/update-publication.dto';
+import { Repository } from 'typeorm';
+import { Publication } from './entities/publication.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PublicationsService {
-  create(createPublicationDto: CreatePublicationDto) {
-    return 'This action adds a new publication';
+  constructor(
+    @InjectRepository (Publication)
+     private readonly publicationRepository:Repository<Publication>,
+  ){}
+
+
+  async create(createPublicationDto: CreatePublicationDto) {
+
+    const publicacion=this.publicationRepository.create(publicacion)
+    await this.publicationRepository.save(Publication)
+    return publicacion;
   }
 
   findAll() {
